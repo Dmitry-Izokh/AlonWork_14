0928d1df0bc7ae26baf663659795519b53c79c01
Дмитрий Изох <izokh_dv@mail.ru>
2022-11-15 21:33:02 +0300

    Добавьте файлы проекта.

---
+++ AlonWork_14/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlonWork_14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Cat cat = new Cat("Алиса", "");
+            string voice = cat.Say();
+
+
+
+            Console.WriteLine(cat.ShowInfo());
+
+            Console.ReadKey();
+
+        }
+    }
+}
